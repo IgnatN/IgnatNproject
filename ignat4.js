4d13eb94bb615297929337ae6d2a5f8456fa1647
IgnatN <84023038+IgnatN@users.noreply.github.com>
2021-07-08 17:38:34 +0300

    Update ignat4.js

--- ignat4.js
+++ ignat4.js
@@ -1,2 +1,25 @@
-$('.nav').css('color', 'red')
-console.log('asx')
+let clock = document.getElementById('time');
+
+let time = setInterval(() => {
+    clock.innerHTML = new Date().toLocaleTimeString()
+}, 1000);
+clock.style.color = 'red';
+clock.style.fontSize = 'large';
+clock.style.fontWeight = 'bold';
+
+
+let converterValue = {};
+converterValue['USD'] = 18.03;
+converterValue['EUR'] = 21.06;
+
+function moneyConverter(value) {
+    output = document.getElementsByClassName("output");
+    for (let i = 0; i < output.length; i++) {
+        for (let currency in converterValue) {
+            if (currency === output[i].id.toUpperCase()) {
+                output[i].innerHTML = value / converterValue[currency];
+            }
+        }
+    }
+
+}
